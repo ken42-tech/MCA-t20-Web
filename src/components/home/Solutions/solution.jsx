ee966dd308983ae97d608bcb510e96ab0b291887
Naresh <naresh.r@turbostart.co>
2025-04-15 20:05:21 +0530

    Updated Footer and Sponsors component

--- src/components/home/Solutions/solution.jsx
+++ src/components/home/Solutions/solution.jsx
@@ -3,116 +3,153 @@ import Image from "next/image";
 
 const Solutions = () => {
   return (
-    <div className="bg-white w-full pt-6 pb-20 px-10">
+    <div className="bg-white w-full pt-6 pb-20 h-[80vh] px-32">
       <div className="px-6 md:pl-24 flex flex-col gap-[4px]">
-        <div className="font-semibold text-4xl md:text-[29.22px] uppercase text-black leading-none">
+        <div className="font-plusjakarta h-[40px]  font-bold text-[44.22px] leading-[85px] tracking-normal uppercase text-black">
           SPONSORSHIP
         </div>
-        <div className="font-thin text-base md:text-sm text-[#616161] leading-none mt-4">
+        <div className="font-plusjakarta font-semibold text-[22px] leading-[85px] tracking-normal text-[#616161]">
           View all Sponsors
         </div>
 
         <div className="mt-8 flex flex-col gap-2">
-          <p className="text-xs font-semibold uppercase pl-2 text-black">
-            Official Partners
+          <p className="font-plusjakarta font-bold text-[13.82px] leading-[22.36px] tracking-normal text-black">
+            OFFICIAL PARTNERS
           </p>
           <div className="flex gap-4 md:gap-6 flex-wrap md:flex-nowrap">
             <Image
               src="/images/home/sponsorship/image 77.svg"
               alt="Partner 1"
-              width={160}
-              height={80}
+              width={213.36}
+              height={134}
             />
             <Image
               src="/images/home/sponsorship/image 78.svg"
               alt="Partner 2"
-              width={160}
-              height={80}
+              width={213.36}
+              height={134}
             />
           </div>
         </div>
       </div>
 
-      <div className="mt-10 px-6 md:px-28 w-full">
-        <div className="flex flex-col md:flex-row justify-between items-start  md:pr-12">
-          <div className="flex flex-col items-center gap-1 ">
-            <p className="text-xs font-semibold uppercase text-black text-center leading-tight">
-              Official Fantasy Game
-            </p>
-            <Image
-              src="/images/home/sponsorship/image 66.svg"
-              alt="Dream11"
-              width={160}
-              height={80}
-            />
-          </div>
+      <div className="mt-10 px-4 md:px-6 lg:px-8 xl:px-12 w-[90%]">
+  <div className="flex flex-col md:flex-row justify-between items-center gap-6 md:gap-0">
+    {/* OFFICIAL FANTASY GAME */}
+    <div className="flex flex-col items-center text-center">
+      <p className="font-plusjakarta font-bold text-[13.82px] leading-[22.36px] tracking-normal text-black mb-4">
+        OFICIAL FANTASY GAME
+      </p>
+      <div className="w-full flex justify-center  items-center h-[134px]">
+        <Image
+          src="/images/home/sponsorship/image 66.svg"
+          alt="Dream11"
+          width={213.36}
+          height={134}
+          className="max-w-full h-auto"
+        />
+      </div>
+    </div>
 
-          <div className="hidden md:block w-px bg-black h-16 self-center" />
+    {/* Vertical line 1 */}
+    <div className="hidden md:flex pt-[40px] items-center h-[134px]">
+      <div className="w-px bg-black h-[109px]"></div>
+    </div>
 
-          <div className="flex flex-col items-center gap-1 px-2">
-            <p className="text-xs font-semibold uppercase text-black text-center leading-tight">
-              Umpire Sponsor
-            </p>
-            <Image
-              src="/images/home/sponsorship/image 67.svg"
-              alt="Skoda"
-              width={160}
-              height={80}
-            />
-          </div>
+    {/* UMPIRE SPONSORS */}
+    <div className="flex flex-col items-center text-center">
+      <p className="font-plusjakarta font-bold text-[13.82px] leading-[22.36px] tracking-normal text-black mb-4">
+        UMPIRE SPONSORS
+      </p>
+      <div className="w-full flex justify-center items-center h-[134px]">
+        <Image
+          src="/images/home/sponsorship/image 67.svg"
+          alt="Skoda"
+          width={213.36}
+          height={134}
+          className="max-w-full h-auto"
+        />
+      </div>
+    </div>
 
-          <div className="hidden md:block w-px bg-black h-16 self-center" />
+    {/* Vertical line 2 */}
+    <div className="hidden pt-[40px] md:flex items-center h-[134px]">
+      <div className="w-px bg-black h-[109px]"></div>
+    </div>
 
-          <div className="flex flex-col items-center gap-1 ">
-            <p className="text-xs font-semibold uppercase text-black text-center leading-tight">
-              Associate Sponsors
-            </p>
-            <div className="flex flex-col md:flex-row gap-4 items-center">
-              <Image
-                src="/images/home/sponsorship/image 69.svg"
-                alt="Atul Projects"
-                width={130}
-                height={80}
-              />
-              <div className="hidden md:block w-px bg-black h-16 self-center" />
-              <Image
-                src="/images/home/sponsorship/image 70.svg"
-                alt="Wardwiz"
-                width={130}
-                height={80}
-              />
-            </div>
-          </div>
-
-          <div className="hidden md:block w-px bg-black h-16 self-center" />
-
-          <div className="flex flex-col items-center gap-1 ">
-            <p className="text-xs font-semibold uppercase text-black text-center leading-tight">
-              Hydration Partner
-            </p>
-            <Image
-              src="/images/home/sponsorship/image 71.svg"
-              alt="Bisleri"
-              width={160}
-              height={80}
-            />
-          </div>
-
-          <div className="hidden md:block w-px bg-black h-16 self-center" />
-
-          <div className="flex flex-col items-center gap-1 ">
-            <p className="text-xs font-semibold uppercase text-black text-center leading-tight">
-              Ticketing Partner
-            </p>
-            <Image
-              src="/images/home/sponsorship/image 74.svg"
-              alt="BookMyShow"
-              width={160}
-              height={80}
-            />
-          </div>
+    {/* ASSOCIATE SPONSORS */}
+    <div className="flex flex-col items-center text-center">
+      <p className="font-plusjakarta font-bold text-[13.82px] leading-[22.36px] tracking-normal text-black mb-4">
+        ASSOCIATE SPONSORS
+      </p>
+      <div className="flex flex-row items-center h-[134px]">
+        <div className="flex justify-center items-center">
+          <Image
+            src="/images/home/sponsorship/image 69.svg"
+            alt="Atul Projects"
+            width={213.36}
+            height={134}
+            className="max-w-full h-auto"
+          />
+        </div>
+        <div className="flex items-center h-[134px]">
+          <div className="w-px bg-black h-[109px] mx-1"></div>
+        </div>
+        <div className="flex justify-center items-center">
+          <Image
+            src="/images/home/sponsorship/image 70.svg"
+            alt="Wardwiz"
+            width={213.36}
+            height={134}
+            className="max-w-full h-auto"
+          />
         </div>
       </div>
+    </div>
+
+    {/* Vertical line 3 */}
+    <div className="hidden pt-[40px] md:flex items-center h-[134px]">
+      <div className="w-px bg-black h-[109px]"></div>
+    </div>
+
+    {/* HYDRATION PARTNER */}
+    <div className="flex flex-col items-center text-center">
+      <p className="font-plusjakarta font-bold text-[13.82px] leading-[22.36px] tracking-normal text-black mb-4">
+        HYDRATION PARTNER
+      </p>
+      <div className="w-full flex justify-center items-center h-[134px]">
+        <Image
+          src="/images/home/sponsorship/image 71.svg"
+          alt="Bisleri"
+          width={213.36}
+          height={134}
+          className="max-w-full h-auto"
+        />
+      </div>
+    </div>
+
+    {/* Vertical line 4 */}
+    <div className="hidden md:flex pt-[40px] items-center h-[134px]">
+      <div className="w-px bg-black h-[109px]"></div>
+    </div>
+
+    {/* TICKETING PARTNER */}
+    <div className="flex flex-col items-center text-center">
+      <p className="font-plusjakarta font-bold text-[13.82px] leading-[22.36px] tracking-normal text-black mb-4">
+        TICKETING PARTNER
+      </p>
+      <div className="w-full flex justify-center items-center h-[134px]">
+        <Image
+          src="/images/home/sponsorship/image 74.svg"
+          alt="BookMyShow"
+          width={213.36}
+          height={134}
+          className="max-w-full h-auto"
+        />
+      </div>
+    </div>
+  </div>
+</div>
     </div>
   );
 };
--- src/components/layout/footer/footer.jsx
+++ src/components/layout/footer/footer.jsx
@@ -4,239 +4,96 @@ import "./style.css";
 
 const Footer = () => {
   return (
-    <div>
-      <div className="bg-black px-8 sm:px-16 md:px-32">
-        <div className="section-width  lg:py-14">
-          <div className="lg:flex justify-between lg:gap-12 max-lg:space-y-12">
-            <div className="max-w-md">
-              <Image
-                src="/images/home/logo.svg"
-                alt="t20mumbai Logo"
-                width={200}
-                height={280}
-                className="mb-6 pl-6"
-              />
-            </div>
-            <div className="flex flex-col lg:flex-row lg:gap-8 space-y-8 lg:space-y-0 mr-8 pl-24">
-              <div className="flex flex-col gap-0.5">
-                <h6 className="text-[#E07E27] text-sm font-normal font-Inter whitespace-nowrap mt-1">
-                  LATEST UPDATES
-                </h6>
-                <ul className="leading-none">
-                  <li>
-                    <Link
-                      href="#"
-                      className="text-[#FFFFFF] font-Inter font-light text-[8px] hover:text-white"
-                    >
-                      Standings
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href="#"
-                      className="text-[#FFFFFF] font-Inter font-light text-[8px] hover:text-white"
-                    >
-                      Fixtures
-                    </Link>
-                  </li>
-                </ul>
-              </div>
+    <div className=  " main bg-black h-[475px] px-8 sm:px-16 md:px-32 flex justify-center items-center gap-[20px]">
+      <div className="footer-main flex justify-between items-center w-[90%] h-[90%] flex-row gap-[60px]">
 
-              {/* Company Links */}
-              <div>
-                <h6 className="text-[#E07E27] text-sm font-normal font-Inter whitespace-nowrap mt-1">
-                  TEAMS
-                </h6>
-                <ul className="leading-none">
-                  <li>
-                    <Link
-                      href="#"
-                      className="text-[#FFFFFF] font-Inter font-light text-[8px] whitespace-nowrap hover:text-white"
-                    >
-                      Aakash Tigers MWS
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href="#"
-                      className="text-[#FFFFFF] font-Inter font-light text-[8px] hover:text-white"
-                    >
-                      Arcs Andheri
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href="#"
-                      className="text-[#FFFFFF] font-Inter font-light text-[8px] hover:text-white"
-                    >
-                      Eagle Thane Strikers
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href="#"
-                      className="text-[#FFFFFF] font-Inter font-light text-[8px] whitespace-nowrap hover:text-white"
-                    >
-                      Namo Bandra Blasters
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href="#"
-                      className="text-[#FFFFFF] font-Inter font-light text-[8px] hover:text-white"
-                    >
-                      Sobo Supersonics
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-              <div>
-                <h6 className="text-[#E07E27] text-sm font-normal font-Inter whitespace-nowrap mt-1">
-                  POINTS TABLE
-                </h6>
-                <ul className="leading-none">
-                  <li>
-                    <Link
-                      href="#"
-                      className="text-[#FFFFFF] font-Inter font-light text-[8px] hover:text-white"
-                    >
-                      Points Table
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href="#"
-                      className="text-[#FFFFFF] font-Inter font-light text-[8px] hover:text-white"
-                    >
-                      Play Offs
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-              <div>
-                <h6 className="text-[#E07E27] text-sm font-normal font-Inter whitespace-nowrap mt-1">
-                  STATISTICS
-                </h6>
-                <ul className="leading-none">
-                  <li>
-                    <Link
-                      href="#"
-                      className="text-[#FFFFFF] font-Inter font-light text-[8px] hover:text-white"
-                    >
-                      Game Stats
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href="#"
-                      className="text-[#FFFFFF] font-Inter font-light text-[8px] hover:text-white"
-                    >
-                      Player Stats
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href="#"
-                      className="text-[#FFFFFF] font-Inter font-light text-[8px] hover:text-white"
-                    >
-                      Overall Statistics
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-              <div>
-                <h6 className="text-[#E07E27] text-sm font-normal font-Inter whitespace-nowrap mt-1 ">
-                  ABOUT US
-                </h6>
-                <ul className="leading-none">
-                  <li>
-                    <Link
-                      href="#"
-                      className="text-[#FFFFFF] font-Inter font-light text-[8px] hover:text-white"
-                    >
-                      About
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href="#"
-                      className="text-[#FFFFFF] font-Inter font-light  text-[8px] hover:text-white"
-                    >
-                      We Stand For
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href="#"
-                      className="text-[#FFFFFF] font-Inter font-light text-[8px]  whitespace-nowrap hover:text-white"
-                    >
-                      Code of Conduct
-                    </Link>
-                  </li>
-                </ul>
-              </div>
+           <div className="footer-col-1 w-[20%] h-full flex justify-center items-start flex-col pt-[20px] gap-[30px] ">
+            <div className="footer-logo">
+            <Image src="/images/footer/t20logo.svg" alt="T20 Mumbai" width={203} height={100} />
             </div>
-            <div className="flex flex-wrap gap-2 justify-center h-8 pt-3 items-center">
-              <Link href="#">
-                <Image
-                  src="/images/footer/Link.svg"
-                  width={110}
-                  height={80}
-                  alt="Zo"
-                />
-              </Link>
-
-              <Link href="#">
-                <Image
-                  src="/images/footer/Image.svg"
-                  width={110}
-                  height={80}
-                  alt="Zom"
-                />
-              </Link>
-            </div>
+            <div className="footer-content text-[#999999] font-plusjakarta font-medium text-[19.47px] leading-[34.07px] tracking-normal">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac neque sit amet sem varius feugiat eget at quam.            </div>
+           </div>
+           <div className="footer-col-2 w-[80%] h-full">
+            <div className="footer-row-1 w-full h-[70%] flex justify-between flex-row pt-[130px]">
+            <div>
+            <h4 className="text-[#E07E27] mb-2 font-inter font-normal text-[20px] leading-[22.5px] tracking-[-0.045em] align-middle uppercase">LATEST UPDATES</h4>
+            <ul className="text-white  text-opacity-90 space-y-2 font-inter font-light text-[11px] leading-[16.5px] tracking-normal align-middle">
+              <li>Standings</li>
+              <li>Fixtures</li>
+            </ul>
           </div>
-          <div className="flex justify-between items-center text-[8px] px-4">
-            <div className="w-1/4 text-left pt-0.5">
-              <p className="not-italic text-sm text-[#BCC3D7] mb-20 pr-9">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-                ac neque sit amet sem varius feugiat eget at quam.
-              </p>
-            </div>
-
-            <div className="text-[8px] font-Inter font-thin text-xs text-center">
-              Terms and Conditions | Privacy Policy | Contact Us
-            </div>
-
-            <div className="flex items-center gap-x-1.5 text-[8px] font-Inter font-thin text-xs">
-              <span>Connect with us:</span>
-              <Image
-                src="/images/footer/mdi_instagram.svg"
-                alt="Instagram Logo"
-                width={17}
-                height={17}
-              />
-              <Image
-                src="/images/footer/line-md_twitter.svg"
-                alt="Twitter Logo"
-                width={17}
-                height={17}
-              />
-              <Image
-                src="/images/footer/line-md_youtube.svg"
-                alt="YouTube Logo"
-                width={17}
-                height={17}
-              />
-            </div>
-
-            <div className="font-Inter font-extralight text-[8px] text-xs text-right pr-18">
-              © Copyright 2025 T20 Mumbai 2025
-            </div>
+          <div>
+            <h4 className="text-[#E07E27] mb-2 font-inter font-normal text-[20px] leading-[22.5px] tracking-[-0.045em] align-middle uppercase">TEAMS</h4>
+            <ul className="text-white text-opacity-90 space-y-2 font-inter font-light text-[11px] leading-[16.5px] tracking-normal align-middle">
+              <li>Aakash Tigers MWS</li>
+              <li>Arcs Andheri</li>
+              <li>Eagle Thane Strikers</li>
+              <li>Namo Bandra Blasters</li>
+              <li>Sobo Supersonics</li>
+            </ul>
           </div>
+          <div>
+            <h4 className="text-[#E07E27]  mb-2 font-inter font-normal text-[20px] leading-[22.5px] tracking-[-0.045em] align-middle uppercase">POINTS TABLE</h4>
+            <ul className="text-white text-opacity-90 space-y-2 font-inter font-light text-[11px] leading-[16.5px] tracking-normal align-middle">
+              <li>Points Table</li>
+              <li>Play Off</li>
+             
+            </ul>
+          </div>
+          <div>
+            <h4 className="text-[#E07E27]  mb-2 font-inter font-normal text-[20px] leading-[22.5px] tracking-[-0.045em] align-middle uppercase">STATISTICS</h4>
+            <ul className="text-white text-opacity-90 space-y-2 font-inter font-light text-[11px] leading-[16.5px] tracking-normal align-middle">
+              <li>Game Stats</li>
+              <li>Player Stats</li>
+              <li>Overall Statistics</li>
+              
+            </ul>
+          </div>
+          <div>
+            <h4 className="text-[#E07E27]  mb-2 font-inter font-normal text-[20px] leading-[22.5px] tracking-[-0.045em] align-middle uppercase">ABOUT US</h4>
+            <ul className="text-white text-opacity-90 space-y-2 font-inter font-light text-[11px] leading-[16.5px] tracking-normal align-middle">
+              <li>About</li>
+              <li>What We Stand For</li>
+              <li>Code of Conduct</li>
+            
+            </ul>
+          </div>
+          <div>
+           <div className="footer-image flex justify-center flex-col gap-[20px] ">
+           <Image src="/images/footer/Appstore.svg" alt="Google" width={150} height={100} />
+           <Image src="/images/footer/Googleplaystore.svg" alt="Appstore" width={150} height={100} />
+           </div>
+          </div>
+            </div>
+            <div className="footer-row-2 w-full h-[30%] flex justify-between items-center flex-row gap-[20px] pt-[50px]">
+            <div className="flex   gap-1">
+            <a href="#" className="hover:text-white font-inter font-bold text-[12px] leading-[18px] tracking-normal align-middle">Terms and Conditions</a>
+            <span>|</span>
+            <a href="#" className="hover:text-white font-inter font-bold text-[12px] leading-[18px] tracking-normal align-middle">Privacy Policy</a>
+            <span>|</span>
+            <a href="#" className="hover:text-white font-inter font-bold text-[12px] leading-[18px] tracking-normal align-middle">Contact Us</a>
+          </div>
+          <div className="flex  gap-1 ">
+          <div className="flex   gap-5">
+            <div className="font-inter font-bold text-[12px] leading-[18px] tracking-normal align-middle">
+             Connect with us:
+            </div>
+            <div className="flex gap-4">
+            <Image src="/images/footer/insta.svg" alt="Google" width={24} height={24} />
+            <Image src="/images/footer/twitter.svg" alt="Google" width={24} height={24} />
+            <Image src="/images/footer/youtube.svg" alt="Google" width={24} height={24} />
+            </div>
+            
+          </div>
+          </div>
+          <div className="font-inter font-bold text-[12px] leading-[18px] tracking-normal align-middle">
+          © Copyright 2025 T20 Mumbai 2025          </div>
+            </div>
+           </div>
+
         </div>
       </div>
-    </div>
   );
 };
 
