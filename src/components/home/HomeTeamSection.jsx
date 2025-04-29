27d28ef6baae934659e62a5271be7b93fa1e3c69
judson-ts <155189292+judson-ts@users.noreply.github.com>
2025-04-29 13:09:01 +0530

    `Refactor HomeTeamSection component to use TeamCard component`

--- src/components/home/HomeTeamSection.jsx
+++ src/components/home/HomeTeamSection.jsx
@@ -1,6 +1,7 @@
 "use client";
 import Image from "next/image";
 import React from "react";
+import TeamCard from "../common/TeamCard";
 
 const teamsDataHomePage = [
   {
@@ -88,44 +89,7 @@ const HomeTeamSection = () => {
           <div className="w-full overflow-x-auto py-8 scrollbar-hide">
             <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 2xl:gap-8 gap-5 ">
               {teamsDataHomePage.map((item, i) => {
-                console.log(item.gradient.from, "first");
-                return (
-                  <div
-                    // bg-gradient-to-b from-[${item.gradient.from}] to-[${item.gradient.to}]
-                    className={`p-4 rounded-xl  flex justify-center relative `}
-                    key={i}
-                    style={{
-                      background: `linear-gradient(to bottom, ${item.gradient.from}, ${item.gradient.to})`,
-                    }}
-                  >
-                    <Image
-                      src={"/images/elements/teamCardRoundElement.png"}
-                      width={100}
-                      height={100}
-                      className="w-full h-full absolute top-0 left-0 rounded-xl opacity-60"
-                      alt="Logo"
-                    />
-                    <Image
-                      src={"/images/elements/teamCardElement.png"}
-                      width={100}
-                      height={100}
-                      className="w-full h-full absolute top-0 left-0 rounded-xl"
-                      alt="Logo"
-                    />
-                    <div className="relative">
-                      <Image
-                        src={item.logo}
-                        width={100}
-                        height={100}
-                        className="w-auto xl:h-32 h-28 mx-auto"
-                        alt="Logo"
-                      />
-                      <p className="text-white text-center mt-10">
-                        {item.team}
-                      </p>
-                    </div>
-                  </div>
-                );
+                return <TeamCard data={item} key={i} />;
               })}
             </div>
           </div>
