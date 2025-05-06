45b6c189aba38ac88b5f7569e6a19560355c6eeb
judson-ts <155189292+judson-ts@users.noreply.github.com>
2025-05-06 14:00:51 +0530

    `Refactored PlayerCard component to include gradient backgrounds and updated styling`

--- src/components/home/Iconplayers.jsx
+++ src/components/home/Iconplayers.jsx
@@ -8,45 +8,73 @@ import { Carousel } from "../Carousel";
 const players = [
   {
     id: 1,
-    name: "Suryakumar Yadav",
-    image: "/images/home/players/player1.png",
-    teamName: "Triumph Knights Mumbai North East",
-    teamLogo: "/images/home/team/ipTriumphKnights.jpg",
-  },
-  {
-    id: 2,
     name: "Shivam Dube",
     image: "/images/home/players/player2.png",
     teamName: "ARCS Andheri",
     teamLogo: "/images/home/team/arcsAndheri.jpg",
+
+    gradient: {
+      from: "#263C90",
+      to: "#8C2B8E",
+    },
   },
   {
-    id: 3,
-    name: "Shreyas Iyer",
-    image: "/images/home/players/player3.png",
-    teamName: "SoBo Mumbai Falcons",
-    teamLogo: "/images/home/team/ipmumbaifalcons.jpg",
-  },
-  {
-    id: 4,
-    name: "Ajinkya Rahane",
-    image: "/images/home/players/player4.png",
-    teamName: "Bandra Blasters",
-    teamLogo: "/images/home/team/ipBandraBlasters.jpg",
-  },
-  {
-    id: 5,
-    name: "Prithvi Shaw",
-    image: "/images/home/players/player5.png",
-    teamName: "North Mumbai Panthers",
-    teamLogo: "/images/home/team/ipNorthMumbaiPanther.jpg",
-  },
-  {
-    id: 6,
+    id: 2,
     name: "Sarfaraz Khan",
     image: "/images/home/players/player6.png",
     teamName: "Aakash Tigers Mumbai Western Suburbs",
     teamLogo: "/images/home/team/aakashTigers.jpg",
+    gradient: {
+      from: "#FD7E00",
+      to: "#0064FF",
+    },
+  },
+  {
+    id: 3,
+
+    name: "Ajinkya Rahane",
+    image: "/images/home/players/player4.png",
+    teamName: "Bandra Blasters",
+    teamLogo: "/images/home/team/ipBandraBlasters.jpg",
+    gradient: {
+      from: "#4B1C86",
+      to: "#E51C21",
+    },
+  },
+  {
+    id: 4,
+
+    name: "Suryakumar Yadav",
+    image: "/images/home/players/player1.png",
+    teamName: "Triumph Knights Mumbai North East",
+    teamLogo: "/images/home/team/ipTriumphKnights.jpg",
+    gradient: {
+      from: "#E8D273",
+      to: "#9E7437",
+    },
+  },
+  {
+    id: 5,
+    name: "Shreyas Iyer",
+    image: "/images/home/players/player3.png",
+    teamName: "SoBo Mumbai Falcons",
+    teamLogo: "/images/home/team/ipmumbaifalcons.jpg",
+    gradient: {
+      from: "#F47B06",
+      to: "#882626",
+    },
+  },
+  {
+    id: 6,
+
+    name: "Prithvi Shaw",
+    image: "/images/home/players/player5.png",
+    teamName: "North Mumbai Panthers",
+    teamLogo: "/images/home/team/ipNorthMumbaiPanther.jpg",
+    gradient: {
+      from: "#FEB713",
+      to: "#5F4200",
+    },
   },
   {
     id: 7,
@@ -54,6 +82,10 @@ const players = [
     image: "/images/home/players/player7.png",
     teamName: "Mumbai South Central Maratha Royals",
     teamLogo: "/images/home/team/ipMarathaRoyals.jpg",
+    gradient: {
+      from: "#1000A1",
+      to: "#B84124",
+    },
   },
   {
     id: 8,
@@ -61,37 +93,68 @@ const players = [
     image: "/images/home/players/player8.png",
     teamName: "Eagle Thane Strikers",
     teamLogo: "/images/home/team/ipEagleThaneStrikers.jpg",
+    gradient: {
+      from: "#FBC92E",
+      to: "#262262",
+    },
   },
 ];
 
 const PlayerCard = ({ data }) => {
   return (
-    <div className="relative   overflow-hidden rounded-lg  bg-[url('/images/home/players/bgTexture.png')] bg-cover bg-center bg-no-repeat flex flex-col">
-      <div className=" w-full relative mt-auto">
-        <div className="w-full  absolute bottom-0 left-0 h-1/3 bg-gradient-to-t from-black to-transparent" />
-        <Image
-          width={200}
-          height={200}
-          src={data?.image}
-          alt={data?.name}
-          className="w-full h-full"
-        />
-        <div className="bg-[#BF5800] border border-[#E07E27] text-white px-4 py-1 rounded-sm font-medium text-sm capitalize w-fit mx-auto absolute bottom-5 left-0 right-0">
-          {data?.name}
-        </div>
-      </div>
-      <div className="bg-[#08113F] w-full">
-        <div className="w-full text-white flex  gap-2  mt-auto bg-gradient-to-r from-transparent via-white/30 to-transparent min-h-[50px]">
-          <div className="bg-white   flex justify-center items-center">
-            <img
-              src={data?.teamLogo}
-              alt="Team Logo"
-              className="w-[50px] h-auto mx-auto max-h-[50px]"
+    <div
+      className="p-[4px] rounded-lg "
+      style={{
+        background:
+          "radial-gradient(circle,rgba(236, 216, 21, 1) 38%, rgba(241, 90, 34, 1) 89%, rgba(241, 90, 34, 1) 100%)",
+      }}
+    >
+      <div
+        className="relative  h-full   rounded-lg    bg-cover bg-center bg-no-repeat flex flex-col"
+        style={{
+          background: `linear-gradient(to bottom, ${data?.gradient?.from} -9.69%, ${data?.gradient?.to}) 88.26%`,
+        }}
+      >
+        <div
+          className="p-[4px] rounded-full  w-fit mx-auto 2xl:-mt-12 xl:-mt-8 -mt-6 "
+          style={{
+            background:
+              "radial-gradient(circle,rgba(236, 216, 21, 1) 38%, rgba(241, 90, 34, 1) 89%, rgba(241, 90, 34, 1) 100%)",
+          }}
+        >
+          <div
+            className="rounded-full 2xl:w-[200px] 2xl:h-[200px] xl:h-[150px] xl:w-[150px] md:h-[120px] md:w-[120px] w-[150px] h-[150px] overflow-hidden relative"
+            style={{
+              background: `linear-gradient(to bottom, ${data?.gradient?.from}, ${data?.gradient?.to})`,
+            }}
+          >
+            <div className="h-full w-full absolute left-0 bottom-0 bg-[#00000050]"></div>
+            {/*  <div className="w-full  absolute bottom-0 left-0 h-1/3 bg-gradient-to-t from-black to-transparent" /> */}
+            <Image
+              width={200}
+              height={200}
+              src={data?.image}
+              alt={data?.name}
+              className="object-cover w-full h-full rounded-full relative"
             />
           </div>
-          <h4 className="text-white 2xl:text-sm text-xs font-semibold py-[6px] flex items-center justify-start uppercase   w-[calc(100%-50px)] text-start">
-            {data?.teamName}
-          </h4>
+        </div>
+        <div className="font-bold text-white px-2 py-6 rounded-sm xl:text-xl text-lg capitalize w-fit mx-auto text-center">
+          {data?.name}
+        </div>
+        <div className=" w-full mt-auto">
+          <div className="w-full text-white flex  gap-4  mt-auto bg-gradient-to-r from-transparent via-white/30 to-transparent min-h-[50px]">
+            <div className="bg-white   flex justify-center items-center">
+              <img
+                src={data?.teamLogo}
+                alt="Team Logo"
+                className="w-[80px] h-auto mx-auto max-h-[80px]"
+              />
+            </div>
+            <h4 className="text-white 2xl:text-lg text-base font-bold py-[6px] flex items-center justify-start uppercase   w-[calc(100%-50px)] text-start">
+              {data?.teamName}
+            </h4>
+          </div>
         </div>
       </div>
     </div>
@@ -102,7 +165,7 @@ export default function IconPlayers() {
   return (
     <div className="section-width padding-top">
       <TitleComponent title={"Icon Players"} />
-      <div className="sm:grid  lg:grid-cols-4 lg:gap-8 md:grid-cols-3 gap-5 sm:grid-cols-2 grid-cols-1 hidden">
+      <div className="sm:grid  lg:grid-cols-4 xl:gap-x-8 xl:gap-y-16  md:grid-cols-3 gap-x-5 gap-y-10 sm:grid-cols-2 grid-cols-1 hidden  pt-8">
         {players.map((player) => (
           <PlayerCard data={player} key={player.id} />
         ))}
@@ -115,7 +178,7 @@ export default function IconPlayers() {
           loop={true}
         >
           {players.map((player, i) => (
-            <SwiperSlide key={i}>
+            <SwiperSlide key={i} style={{ paddingTop: "20px" }}>
               <PlayerCard data={player} key={i} />
             </SwiperSlide>
           ))}
