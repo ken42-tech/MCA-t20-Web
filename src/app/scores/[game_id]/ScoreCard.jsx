3d55938216802abc696b2cc9e18c335f89edc22d
SurajRai16 <suraj.r@turbostart.co>
2025-05-19 17:16:36 +0530

    refactor: enhance layout and structure of ScoreCard component

--- src/app/scores/[game_id]/ScoreCard.jsx
+++ src/app/scores/[game_id]/ScoreCard.jsx
@@ -74,7 +74,7 @@ function ScoreCard({ match }) {
       <div className=" section-width">
         <div className="bg-[url('/images/scorecard/bgcardmain.svg')] bg-cover text-white mt-10 sm:mt-20 rounded-lg">
           <div className="bg-[#203376] text-center py-1 rounded-lg">
-            <span className="text-sm font-medium uppercase p-2">
+            <span className="text-sm font-medium uppercase">
               {match.Matchdetail.Equation}
             </span>
           </div>
@@ -138,118 +138,137 @@ function ScoreCard({ match }) {
           </div>
         </div>
 
-        {/* Tabs for switching innings */}
-        <div className="flex justify-center mt-10 bg-transparent rounded-t-lg bg-gradient-to-r from-[#203376] via-black to-[#203376] overflow-x-auto min-w-[800px]">
-          <button
-            onClick={() => switchTab("home")}
-            className={`flex-1 px-4 py-2 text-center ${
-              activeInnings === "home"
-                ? "text-orange-500 border-b-4 border-orange-500"
-                : "bg-transparent text-white"
-            }`}
-          >
-            {homeTeamName}
-          </button>
-          <button
-            onClick={() => switchTab("away")}
-            className={`flex-1 px-4 py-2 text-center ${
-              activeInnings === "away"
-                ? "text-orange-500 border-b-4 border-orange-500"
-                : "bg-transparent text-white"
-            }`}
-          >
-            {awayTeamName}
-          </button>
-        </div>
+        <div className="w-full overflow-x-auto ">
+          <div className="w-full">
+            <div className="min-w-[1000px]">
+              {/* Tabs for switching innings */}
+              <div className="flex justify-center mt-10 bg-transparent rounded-t-lg bg-gradient-to-r from-[#203376] via-black to-[#203376] min-w-[800px]">
+                <button
+                  onClick={() => switchTab("home")}
+                  className={`flex-1 px-4 py-2 text-center ${
+                    activeInnings === "home"
+                      ? "text-orange-500 border-b-4 border-orange-500"
+                      : "bg-transparent text-white"
+                  }`}
+                >
+                  {homeTeamName}
+                </button>
+                <button
+                  onClick={() => switchTab("away")}
+                  className={`flex-1 px-4 py-2 text-center ${
+                    activeInnings === "away"
+                      ? "text-orange-500 border-b-4 border-orange-500"
+                      : "bg-transparent text-white"
+                  }`}
+                >
+                  {awayTeamName}
+                </button>
+              </div>
 
-        {/* Scorecard */}
-        <div className="bg-[#0F1A2D]  shadow-md mb-8 overflow-x-auto min-w-[800px] rounded-b-lg">
-          <div className="flex items-center bg-[#0F1A2D] text-white px-6 py-4">
-            <div className="flex items-center space-x-3 flex-1">
-              <Image
-                src={
-                  activeInnings === "home"
-                    ? `/images/scorecard/${homeTeamName}.svg`
-                    : `/images/scorecard/${awayTeamName}.svg`
-                }
-                alt={activeInnings === "home" ? homeTeamName : awayTeamName}
-                width={100}
-                height={100}
-              />
-              <span className="uppercase font-medium">
-                {activeInnings === "home" ? homeTeamName : awayTeamName}
-              </span>
-            </div>
-            <div className="text-sm">
-              <span className="font-semibold">
-                {(activeInnings === "home" ? homeInnings : awayInnings).Total}/
-                {(activeInnings === "home" ? homeInnings : awayInnings).Wickets}
-              </span>
-              <span className="ml-2">
-                ({(activeInnings === "home" ? homeInnings : awayInnings).Overs}{" "}
-                Overs)
-              </span>
+              {/* Scorecard */}
+              <div className="bg-[#0F1A2D]  shadow-md mb-8 rounded-b-lg">
+                <div className="flex items-center bg-[#0F1A2D] text-white px-6 py-4">
+                  <div className="flex items-center space-x-3 flex-1">
+                    <Image
+                      src={
+                        activeInnings === "home"
+                          ? `/images/scorecard/${homeTeamName}.svg`
+                          : `/images/scorecard/${awayTeamName}.svg`
+                      }
+                      alt={
+                        activeInnings === "home" ? homeTeamName : awayTeamName
+                      }
+                      width={100}
+                      height={100}
+                    />
+                    <span className="uppercase font-medium">
+                      {activeInnings === "home" ? homeTeamName : awayTeamName}
+                    </span>
+                  </div>
+                  <div className="text-sm">
+                    <span className="font-semibold">
+                      {
+                        (activeInnings === "home" ? homeInnings : awayInnings)
+                          .Total
+                      }
+                      /
+                      {
+                        (activeInnings === "home" ? homeInnings : awayInnings)
+                          .Wickets
+                      }
+                    </span>
+                    <span className="ml-2">
+                      (
+                      {
+                        (activeInnings === "home" ? homeInnings : awayInnings)
+                          .Overs
+                      }{" "}
+                      Overs)
+                    </span>
+                  </div>
+                </div>
+
+                {/* Batting Table */}
+                <div className="overflow-x-auto bg-[#0F1A2D] text-white px-6 py-4 rounded-b-lg">
+                  <div className="uppercase font-bold mb-2">Batting</div>
+                  <table className="w-full text-left text-sm border-collapse">
+                    <thead>
+                      <tr className="border-b border-gray-700">
+                        <th className="py-2">Batsman</th>
+                        <th className="py-2">Dismissal</th>
+                        <th className="py-2">R</th>
+                        <th className="py-2">B</th>
+                        <th className="py-2">4S</th>
+                        <th className="py-2">6S</th>
+                        <th className="py-2">S/R</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {batting
+                        .filter((p) => p.Howout)
+                        .map((p, i) => (
+                          <tr key={i} className="border-b border-gray-800">
+                            <td className="py-2">
+                              {
+                                teams[
+                                  activeInnings === "home" ? homeId : awayId
+                                ].Players[p.Batsman]?.Name_Full
+                              }
+                            </td>
+                            <td className="py-2">{p.Howout}</td>
+                            <td className="py-2">{p.Runs}</td>
+                            <td className="py-2">{p.Balls}</td>
+                            <td className="py-2">{p.Fours}</td>
+                            <td className="py-2">{p.Sixes}</td>
+                            <td className="py-2">{p.Strikerate}</td>
+                          </tr>
+                        ))}
+                    </tbody>
+                  </table>
+                </div>
+
+                {/* Did Not Bat */}
+                {didNotBat.length > 0 && (
+                  <div className="bg-[#0F1A2D] text-white px-6 py-4 border-t border-gray-700 ">
+                    <div className="uppercase font-bold mb-2">Did Not Bat</div>
+                    <ul className="text-sm">
+                      {didNotBat.map((p, i) => (
+                        <li key={i} className="py-2">
+                          {
+                            teams[activeInnings === "home" ? homeId : awayId]
+                              .Players[p.Batsman]?.Name_Full
+                          }
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                )}
+              </div>
             </div>
           </div>
-
-          {/* Batting Table */}
-          <div className="overflow-x-auto bg-[#0F1A2D] text-white px-6 py-4">
-            <div className="uppercase font-bold mb-2">Batting</div>
-            <table className="w-full text-left text-sm border-collapse">
-              <thead>
-                <tr className="border-b border-gray-700">
-                  <th className="py-2">Batsman</th>
-                  <th className="py-2">Dismissal</th>
-                  <th className="py-2">R</th>
-                  <th className="py-2">B</th>
-                  <th className="py-2">4S</th>
-                  <th className="py-2">6S</th>
-                  <th className="py-2">S/R</th>
-                </tr>
-              </thead>
-              <tbody>
-                {batting
-                  .filter((p) => p.Howout)
-                  .map((p, i) => (
-                    <tr key={i} className="border-b border-gray-800">
-                      <td className="py-2">
-                        {
-                          teams[activeInnings === "home" ? homeId : awayId]
-                            .Players[p.Batsman]?.Name_Full
-                        }
-                      </td>
-                      <td className="py-2">{p.Howout}</td>
-                      <td className="py-2">{p.Runs}</td>
-                      <td className="py-2">{p.Balls}</td>
-                      <td className="py-2">{p.Fours}</td>
-                      <td className="py-2">{p.Sixes}</td>
-                      <td className="py-2">{p.Strikerate}</td>
-                    </tr>
-                  ))}
-              </tbody>
-            </table>
-          </div>
-
-          {/* Did Not Bat */}
-          {didNotBat.length > 0 && (
-            <div className="bg-[#0F1A2D] text-white px-6 py-4 border-t border-gray-700">
-              <div className="uppercase font-bold mb-2">Did Not Bat</div>
-              <ul className="text-sm">
-                {didNotBat.map((p, i) => (
-                  <li key={i} className="py-2">
-                    {
-                      teams[activeInnings === "home" ? homeId : awayId].Players[
-                        p.Batsman
-                      ]?.Name_Full
-                    }
-                  </li>
-                ))}
-              </ul>
-            </div>
-          )}
         </div>
 
-        <div className="my-20">
+        <div className="sm:my-20 my-10">
           {/* Fall of Wickets */}
           {fallOfWickets.length > 0 && (
             <div className="bg-[#E5EBFF] px-4 rounded-lg pb-5">
@@ -286,61 +305,77 @@ function ScoreCard({ match }) {
           )}
         </div>
 
-        {/* Bowling Stats */}
-        <div className="bg-blue-900 text-white rounded-lg mb-10 overflow-x-auto min-w-[800px]">
-          <div className="flex items-center bg-[url('/images/scorecard/bgcard.svg')] rounded-t-lg">
-            <Image
-              src={
-                activeInnings === "home"
-                  ? `/images/scorecard/${awayTeamName}.svg`
-                  : `/images/scorecard/${homeTeamName}.svg`
-              }
-              alt={activeInnings === "home" ? homeTeamName : awayTeamName}
-              width={100}
-              height={100}
-              className="my-5"
-            />
-            <h3 className="text-xl font-semibold mb-4">{oppositeTeamName}</h3>
-          </div>
-          <div className="overflow-x-auto px-3 sm:px-6 bg-[#0F1A2D] rounded-b-lg ">
-            <table className="table-fixed w-full">
-              <thead>
-                <tr>
-                  <th className="py-2 px-4 text-center">Bowler</th>
-                  <th className="py-2 px-4 text-center">O</th>
-                  <th className="py-2 px-4 text-center">M</th>
-                  <th className="py-2 px-4 text-center">R</th>
-                  <th className="py-2 px-4 text-center">W</th>
-                  <th className="py-2 px-4 text-center">NB</th>
-                  <th className="py-2 px-4 text-center">WD</th>
-                  <th className="py-2 px-4 text-center">E/R</th>
-                </tr>
-              </thead>
-              <tbody>
-                {(activeInnings === "home"
-                  ? homeInnings
-                  : awayInnings
-                ).Bowlers?.map((bowler, index) => (
-                  <tr key={index}>
-                    <td className="py-2 px-4 text-center">
-                      {
-                        teams[activeInnings === "home" ? awayId : homeId]
-                          .Players[bowler.Bowler]?.Name_Full
-                      }
-                    </td>
-                    <td className="py-2 px-4 text-center">{bowler.Overs}</td>
-                    <td className="py-2 px-4 text-center">{bowler.Maidens}</td>
-                    <td className="py-2 px-4 text-center">{bowler.Runs}</td>
-                    <td className="py-2 px-4 text-center">{bowler.Wickets}</td>
-                    <td className="py-2 px-4 text-center">{bowler.Noballs}</td>
-                    <td className="py-2 px-4 text-center">{bowler.Wides}</td>
-                    <td className="py-2 px-4 text-center">
-                      {bowler.Economyrate}
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+        <div className="w-full overflow-x-auto">
+          <div className="w-full">
+            {/* Bowling Stats */}
+            <div className="bg-blue-900 text-white rounded-lg mb-10 min-w-[1000px]">
+              <div className="flex items-center bg-[url('/images/scorecard/bgcard.svg')] rounded-t-lg">
+                <Image
+                  src={
+                    activeInnings === "home"
+                      ? `/images/scorecard/${awayTeamName}.svg`
+                      : `/images/scorecard/${homeTeamName}.svg`
+                  }
+                  alt={activeInnings === "home" ? homeTeamName : awayTeamName}
+                  width={100}
+                  height={100}
+                  className="my-5"
+                />
+                <h3 className="text-xl font-semibold mb-4">
+                  {oppositeTeamName}
+                </h3>
+              </div>
+              <div className=" px-3 sm:px-6 bg-[#0F1A2D] rounded-b-lg ">
+                <table className="table-fixed w-full">
+                  <thead>
+                    <tr>
+                      <th className="py-2 px-4 text-center">Bowler</th>
+                      <th className="py-2 px-4 text-center">O</th>
+                      <th className="py-2 px-4 text-center">M</th>
+                      <th className="py-2 px-4 text-center">R</th>
+                      <th className="py-2 px-4 text-center">W</th>
+                      <th className="py-2 px-4 text-center">NB</th>
+                      <th className="py-2 px-4 text-center">WD</th>
+                      <th className="py-2 px-4 text-center">E/R</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {(activeInnings === "home"
+                      ? homeInnings
+                      : awayInnings
+                    ).Bowlers?.map((bowler, index) => (
+                      <tr key={index}>
+                        <td className="py-2 px-4 text-center">
+                          {
+                            teams[activeInnings === "home" ? awayId : homeId]
+                              .Players[bowler.Bowler]?.Name_Full
+                          }
+                        </td>
+                        <td className="py-2 px-4 text-center">
+                          {bowler.Overs}
+                        </td>
+                        <td className="py-2 px-4 text-center">
+                          {bowler.Maidens}
+                        </td>
+                        <td className="py-2 px-4 text-center">{bowler.Runs}</td>
+                        <td className="py-2 px-4 text-center">
+                          {bowler.Wickets}
+                        </td>
+                        <td className="py-2 px-4 text-center">
+                          {bowler.Noballs}
+                        </td>
+                        <td className="py-2 px-4 text-center">
+                          {bowler.Wides}
+                        </td>
+                        <td className="py-2 px-4 text-center">
+                          {bowler.Economyrate}
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
           </div>
         </div>
       </div>
