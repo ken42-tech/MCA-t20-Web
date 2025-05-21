21752afc41f2badfdc5b24ff2846bbbd77d398da
SurajRai16 <suraj.r@turbostart.co>
2025-05-21 10:48:50 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -21,7 +21,7 @@ export default function Home() {
         <Socials />
         <Gallery />
         <TopPlayers />
-        <HomeStandingsSection />
+    {/* <HomeStandingsSection /> */}
 
         {/* <News /> */}
 
