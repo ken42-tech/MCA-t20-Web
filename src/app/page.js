f3e1de3237e5a8b5358bdcbedaac8b81cd75f0df
judson-ts <155189292+judson-ts@users.noreply.github.com>
2025-05-21 10:58:24 +0530

    `Removed TopPlayers and HomeStandingsSection components, commented out, and reordered their positions in the Home function`

--- src/app/page.js
+++ src/app/page.js
@@ -20,8 +20,8 @@ export default function Home() {
         <IconPlayers />
         <Socials />
         <Gallery />
-        <TopPlayers />
-    {/* <HomeStandingsSection /> */}
+        {/* <TopPlayers /> */}
+        {/* <HomeStandingsSection /> */}
 
         {/* <News /> */}
 
