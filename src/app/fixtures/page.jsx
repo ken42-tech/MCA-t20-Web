d51fa266290689979b5af9ec07abfd94dcb88cda
judson-ts <155189292+judson-ts@users.noreply.github.com>
2025-05-22 17:17:03 +0530

    `Update banner image URL in Page component to use mobile background by default, with desktop background on medium and large screens.`

--- src/app/fixtures/page.jsx
+++ src/app/fixtures/page.jsx
@@ -121,7 +121,7 @@ export default function Page() {
         // heading="Season 3 Fixtures"
         // subheading="Player Profile"
       /> */}
-      <div className="w-full relative flex justify-end py-36  bg-[url('/images/banner/fixture.jpg')] bg-cover bg-right bg-no-repeat ">
+      <div className="w-full relative flex justify-end py-36 bg-[url('/images/banner/mobileBgFixture.jpg')]  sm:bg-[url('/images/banner/fixture.jpg')] bg-cover sm:bg-right bg-center bg-no-repeat ">
         <div className="w-full h-full bg-[#00000080] absolute left-0 top-0 "></div>
         <div className="relative z-10  pt-8 h-full  flex-col overflow-hidden justify-between text-white flex gap-24 mt-52 section-width">
           <div className="w-full flex flex-col items-start justify-between bg-transparent gap-20"></div>
