773d8a5780646790df45497daa2c52674d691bba
judson-ts <155189292+judson-ts@users.noreply.github.com>
2025-05-24 10:56:04 +0530

    `feat: updated hero section with swiper carousel and countdown timer`

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,6 +15,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body>
         <MixpanelProvider />
         <Navbar />
--- src/components/home/hero.jsx
+++ src/components/home/hero.jsx
@@ -2,6 +2,11 @@
 import Image from "next/image";
 import { useState } from "react";
 import CountdownTimer from "./CountdownTimer";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import { FaLocationDot } from "react-icons/fa6";
 
 const Hero = () => {
   const [showVideo, setShowVideo] = useState(false);
@@ -10,63 +15,162 @@ const Hero = () => {
   const closeVideo = () => setShowVideo(false);
 
   return (
-    <>
-      <div className="w-full bg-[url('/images/home/hero/heroImgRohitPattern.png')] bg-cover bg-center  relative pt-32 overflow-hidden">
-        {/* <Image
-          src="/images/home/bg2.png"
-          alt="background"
-          fill
-          className="object-cover z-0"
-        /> */}
-        <div className="section-width lg:pt-52 md:pt-32 sm:pt-24 pb-32">
-          <div className="flex flex-col items-center sm:items-start gap-3 lg:gap-6">
-            <p className="font-bold text-sm md:text-base xl:text-xl">
-              T20 Mumbai Season 3
-            </p>
+    <div className="xl:h-[800px] lg:h-[700px] md:h-[600px] h-[500px]">
+      <Swiper
+        modules={[Autoplay]}
+        autoplay={{ delay: 300000 }}
+        pagination={{ clickable: true }}
+        loop
+        className="h-full"
+      >
+        <SwiperSlide>
+          <div className="w-full h-full bg-[url('/images/home/hero/slider2.jpg')] bg-cover bg-center  relative md:pt-32 pt-10 overflow-hidden flex justify-center items-center">
+            <div className="section-width  ">
+              <div className="flex md:flex-row flex-col items-start gap-3 lg:gap-6 h-full w-fit">
+                <div className="lg:flex-[40%] flex-1 md:border-r-2 border-white">
+                  <div className="md:w-[80%] max-md:max-w-[150px]  ml-auto">
+                    <img
+                      src="/images/home/hero/ticketlivetext.png"
+                      className="md:w-5/6 lg:w-full h-auto "
+                      alt="text"
+                    />
+                    <button
+                      className="btn-primary  gap-4 items-center text-xs md:flex hidden"
+                      // onClick={openVideo}
+                    >
+                      BUY TICKETS
+                      <span>
+                        <Image
+                          src="/images/home/hero/buttonIcon.svg"
+                          alt="button-icon"
+                          width={24}
+                          height={24}
+                          className="w-5 h-5"
+                        />
+                      </span>
+                    </button>
+                  </div>
+                </div>
+                <div className="lg:flex-[60%] flex-1 my-auto oswald-font">
+                  <h3 className="  font-extrabold max-w-3xl  uppercase">
+                    From june 4th to June 12TH
+                  </h3>
+                  <div className="flex items-center gap-2 md:mt-3 mt-1">
+                    <FaLocationDot />
+                    <h3 className="  font-extrabold max-w-3xl  uppercase">
+                      wankhede stadium, MUMBAI
+                    </h3>
+                  </div>
+                  <div className="flex items-center gap-2 md:mt-3 mt-1">
+                    <FaLocationDot />
+                    <h3 className="  font-extrabold max-w-3xl ">
+                      DY PATIL STADIUM, NAVI MUMBAI
+                    </h3>
+                  </div>
+                  <div className="flex items-center gap-2 md:mt-3 mt-2">
+                    <h3 className="  font-extrabold max-w-3xl ">
+                      OFFICIALLY AT
+                    </h3>
+                    <img
+                      src="/images/home/hero/districtLogo.png"
+                      className="md:h-20 h-14 w-atuo"
+                      alt=""
+                    />
+                  </div>
+                </div>
+                <button
+                  className="btn-primary  gap-4 items-center text-xs md:hidden flex "
+                  // onClick={openVideo}
+                >
+                  BUY TICKETS
+                  <span>
+                    <Image
+                      src="/images/home/hero/buttonIcon.svg"
+                      alt="button-icon"
+                      width={24}
+                      height={24}
+                      className="w-5 h-5"
+                    />
+                  </span>
+                </button>
+              </div>
 
-            <h1 className="  font-extrabold max-w-3xl max-sm:text-center">
-              Not just back. <br /> Back to build the next icon
-            </h1>
-            <p className="font-bold text-lg md:text-xl xl:text-2xl">
-              #AalaReAglaStar
-            </p>
-            <button
-              className="btn-primary flex gap-4 items-center"
-              onClick={openVideo}
-            >
-              View Details
-              <span>
-                <Image
-                  src="/images/home/hero/buttonIcon.svg"
-                  alt="button-icon"
-                  width={24}
-                  height={24}
-                  className="w-5 h-5"
-                />
-              </span>
-            </button>
-          </div>
+              <div className="absolute bottom-40 -right-0 hidden lg:block">
+                <div className="w-[250px] rounded-l-xl md:rounded-l-xl border-y-2 border-l-2  border-[#E07E27] shadow-2xl overflow-hidden ">
+                  <div
+                    className="  w-full flex overflow-hidden items-center justify-evenly rounded-tl-lg gap-4"
+                    style={{
+                      background:
+                        "linear-gradient(0deg, rgba(0, 0, 0, 0.00) 0%, rgba(0, 0, 0, 0.48) 1.45%, rgba(0, 0, 0, 0.70) 100%), rgba(255, 255, 255, 0.09)",
+                    }}
+                  >
+                    <div className="flex flex-col justify-between  py-6 px-8">
+                      <p className="text-[#E07E27] text-base  xl:text-lg font-semibold leading-3 uppercase">
+                        Season starts in
+                      </p>
+                    </div>
+                  </div>
 
-          <div className="absolute bottom-40 -right-0 hidden sm:block">
-            <div className="w-full rounded-l-xl md:rounded-l-xl border-y-2 border-l-2  border-[#E07E27] shadow-2xl overflow-hidden">
-              <div
-                className="  w-full flex overflow-hidden items-center justify-evenly rounded-tl-lg gap-4"
-                style={{
-                  background:
-                    "linear-gradient(0deg, rgba(0, 0, 0, 0.00) 0%, rgba(0, 0, 0, 0.48) 1.45%, rgba(0, 0, 0, 0.70) 100%), rgba(255, 255, 255, 0.09)",
-                }}
-              >
-                <div className="flex flex-col justify-between  py-6 px-12">
-                  <p className="text-[#E07E27] text-base  xl:text-lg font-semibold leading-3 uppercase">
-                    Season starts in
-                  </p>
+                  <CountdownTimer targetDate="2025-06-04T00:00:00+05:30" />
                 </div>
               </div>
-              <CountdownTimer targetDate="2025-06-04T00:00:00+05:30" />
             </div>
           </div>
-        </div>
-      </div>
+        </SwiperSlide>
+        <SwiperSlide className="h-full">
+          <div className="w-full h-full bg-[url('/images/home/hero/heroImgRohitPattern.png')] bg-cover bg-center  relative pt-32 overflow-hidden flex justify-center items-center">
+            <div className="section-width ">
+              <div className="flex flex-col items-center sm:items-start gap-3 lg:gap-6">
+                <p className="font-bold text-sm md:text-base xl:text-xl">
+                  T20 Mumbai Season 3
+                </p>
+
+                <h1 className="  font-extrabold max-w-3xl max-sm:text-center">
+                  Not just back. <br /> Back to build the next icon
+                </h1>
+                <p className="font-bold text-lg md:text-xl xl:text-2xl">
+                  #AalaReAglaStar
+                </p>
+                <button
+                  className="btn-primary flex gap-4 items-center"
+                  onClick={openVideo}
+                >
+                  View Details
+                  <span>
+                    <Image
+                      src="/images/home/hero/buttonIcon.svg"
+                      alt="button-icon"
+                      width={24}
+                      height={24}
+                      className="w-5 h-5"
+                    />
+                  </span>
+                </button>
+              </div>
+
+              <div className="absolute bottom-40 -right-0 hidden md:block">
+                <div className="w-full rounded-l-xl md:rounded-l-xl border-y-2 border-l-2  border-[#E07E27] shadow-2xl overflow-hidden">
+                  <div
+                    className="  w-full flex overflow-hidden items-center justify-evenly rounded-tl-lg gap-4"
+                    style={{
+                      background:
+                        "linear-gradient(0deg, rgba(0, 0, 0, 0.00) 0%, rgba(0, 0, 0, 0.48) 1.45%, rgba(0, 0, 0, 0.70) 100%), rgba(255, 255, 255, 0.09)",
+                    }}
+                  >
+                    <div className="flex flex-col justify-between  py-6 px-12">
+                      <p className="text-[#E07E27] text-base  xl:text-lg font-semibold leading-3 uppercase">
+                        Season starts in
+                      </p>
+                    </div>
+                  </div>
+                  <CountdownTimer targetDate="2025-06-04T00:00:00+05:30" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+
       {showVideo && (
         <div className="fixed inset-0 z-[9999] flex items-center justify-center bg-black bg-opacity-80">
           <div className="relative w-[90%] md:w-[70%] lg:w-[60%] aspect-video bg-black rounded-lg overflow-hidden">
@@ -88,7 +192,7 @@ const Hero = () => {
           </div>
         </div>
       )}
-    </>
+    </div>
   );
 };
 
